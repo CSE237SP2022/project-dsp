84f36adeafe49f82bdf992d43b13483237a34e60
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2022-03-28 14:25:40 -0500

    simplified majors and minors

--- Student.java
+++ Student.java
@@ -7,45 +7,52 @@ public class Student {
 	private String name;
 	private int gradYear;
 	private int initYear;
-	private int points;
 	
 	private boolean isActive;
 	private boolean isFall;
 	private boolean voteElgible;
 	
 	private String school;
-	private String major1;
-	private String major2;
-	private String minor1;
-	private String minor2;
-	
+	private List<String> majors;
+	private List<String> minors;	
 	private List<Role> resume;
 	
-	
-	public Student(String name, int gradYear, int initYear, boolean isActive, boolean isFall, boolean voteEligible, String school, String major1, String major2, String minor1, String minor2) {
+	public Student(String name, int gradYear, int initYear, boolean isActive, boolean isFall, boolean voteElgible, String school, String major1, String major2, String minor1, String minor2) {
+		resume = new ArrayList<Role>();
+		this.majors = new ArrayList<String>();
+		this.minors = new ArrayList<String>();
+		
 		this.name = name;
 		this.gradYear = gradYear;
 		this.initYear = initYear;
-		this.points = 0;
 		this.isActive = isActive;
 		this.isFall = isFall;
-		this.voteElgible = voteEligible;
+		this.voteElgible = voteElgible;
 		this.school = school;
-		this.major1 = major1;
-		this.major2 = major2;
-		this.minor1 = minor1;
-		this.minor2 = minor2;
-		resume = new ArrayList<Role>();
+		this.majors.add(major1);
+		this.majors.add(major2);
+		this.minors.add(minor1);
+		this.minors.add(minor2);
 	}
+	
+	public Student(String name, int gradYear, int initYear, boolean isActive, boolean isFall, boolean voteElgible, String school, List<String> majors, List<String> minors) {
+		resume = new ArrayList<Role>();
+		
+		this.majors = majors;
+		this.minors = minors;
+		this.name = name;
+		this.gradYear = gradYear;
+		this.initYear = initYear;
+		this.isActive = isActive;
+		this.isFall = isFall;
+		this.voteElgible = voteElgible;
+		this.school = school;
+	}
+	
 	public String getName() {
 		return this.name;
 	}
 	public int getGradYear() {
-		return this.gradYear;
-	}
-	
-	public int changeGradYear(int newGradYear) {
-		this.gradYear = newGradYear;
 		return this.gradYear;
 	}
 	
@@ -69,20 +76,59 @@ public class Student {
 		return this.school;
 	}
 	
+	public List<String> getMajors(){
+		return this.majors;
+	}
+	
+	public List<String> getMinors(){
+		return this.minors;
+	}
+	
+	public int changeGradYear(int newGradYear) {
+		this.gradYear = newGradYear;
+		return this.gradYear;
+	}
+	
 	public String changeSchool(String newSchool) {
 		this.school = newSchool;
 		return this.school;
 	}
 	
-	public String[] getMajorsMinors() {
-		String[] majorsMinors= {this.major1, this.major2, this.minor1, this.minor2};
-		return majorsMinors;
+	public List<String> removeMajor(String removeMajor){
+		this.majors.remove(removeMajor);
+		return this.majors;
+	}
+	
+	public List<String> removeMajor(int majorIndex){
+		this.majors.remove(majorIndex);
+		return this.majors;
+	}
+	
+	public List<String> removeMinor(String removeMinor){
+		this.minors.remove(removeMinor);
+		return this.minors;
+	}
+	
+	public List<String> removeMinor(int minorIndex){
+		this.minors.remove(minorIndex);
+		return this.minors;
+	}
+	
+	public boolean changeActiveStatus(boolean isActive) {
+		this.isActive = isActive;
+		return this.isActive;
+	}
+	
+	public boolean changeCanVote(boolean canVote) {
+		this.voteElgible = canVote;
+		return this.voteElgible;
 	}
 	
 	public void displayAllInfo() {
 		System.out.println("Name: " + this.getName());
 		System.out.println("School: " + this.getSchool());
-		System.out.println("Majors/Minors: " + this.getMajorsMinors());
+		System.out.println("Majors: " + this.getMajors());
+		System.out.println("Minors: " + this.getMinors());
 		System.out.println("Graduation Year: " + this.getGradYear());
 		System.out.println("Initiation Year: " + this.getInitYear());
 		System.out.println("Brother is active: " + this.getIsActive());
